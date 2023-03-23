9d46ad2b41a2bc9d9a712331a2cb80963e9154cf
VasylMolnar <vasylmolnar281@gmail.com>
2023-03-23 15:25:53 +0200

    new

--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -1,24 +1,55 @@
-import React from 'react';
+import { useState, React } from 'react';
 import ReactDOM from 'react-dom';
 import { Formik, FastField, ErrorMessage } from 'formik';
-import { userLoginSchema } from '../../utils/validationSchema';
+import {
+  userLoginSchema,
+  userRegisterSchema,
+} from '../../utils/validationSchema';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import PasswordIcon from '@mui/icons-material/Password';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const AuthModal = ({ isLogIn, setIsLogIn }) => {
+  const [changeContent, setChangeContent] = useState(true);
+
   return ReactDOM.createPortal(
     <div className={isLogIn ? 'backdropAuth' : 'backdropAuth is-hidden'}>
       <div className="authModal">
+        <button
+          type="submit"
+          className="btn cancelIcon"
+          onClick={() => setIsLogIn(false)}
+        >
+          <CancelIcon />
+        </button>
+
         <div className="btn-list">
-          <button className="btn btn-primary">LogIn</button>
-          <button className="btn btn-primary">Register</button>
+          <button
+            className={
+              changeContent ? 'btn btn-warning' : 'btn btn-outline-primary'
+            }
+            onClick={() => setChangeContent(true)}
+          >
+            LogIn
+          </button>
+          <button
+            className={
+              !changeContent ? 'btn btn-warning' : 'btn btn-outline-primary'
+            }
+            onClick={() => setChangeContent(false)}
+          >
+            Register
+          </button>
         </div>
 
-        <div className="authModal_content">
+        <div
+          className={changeContent ? 'authModal_logIn' : 'authModal_register'}
+          style={{ display: 'flex' }}
+        >
           <div className="section_logIn">
             <Formik
-              initialValues={{ name: '', email: '', password: '' }}
+              initialValues={{ email: '', password: '' }}
               onSubmit={(values, { setSubmitting }) => {
                 console.log(values);
               }}
@@ -27,20 +58,6 @@ const AuthModal = ({ isLogIn, setIsLogIn }) => {
               {({ values, handleChange, handleSubmit, isSubmitting }) => (
                 <form onSubmit={handleSubmit} className="logIn">
                   <h1 className="title">Вхід у обліковий запис</h1>
-                  <label htmlFor="name" className="label">
-                    <AccountCircleIcon className="icon" />
-
-                    <FastField
-                      type="text"
-                      name="name"
-                      placeholder="Ваше імя:"
-                    />
-                    <ErrorMessage
-                      name="name"
-                      component="div"
-                      style={{ color: 'red', textTransform: 'upperCase' }}
-                    />
-                  </label>
 
                   <label htmlFor="email" className="label">
                     <MailOutlineIcon className="icon" />
@@ -72,6 +89,7 @@ const AuthModal = ({ isLogIn, setIsLogIn }) => {
                     className="btn btn-outline-primary"
                     type="submit"
                     disabled={isSubmitting}
+                    style={{ width: '100%' }}
                   >
                     Увійти
                   </button>
@@ -80,13 +98,71 @@ const AuthModal = ({ isLogIn, setIsLogIn }) => {
             </Formik>
           </div>
 
-          {/* <div className="section_register">
-            <h1>register</h1>
+          <div className="section_register">
+            <Formik
+              initialValues={{ name: '', email: '', password: '' }}
+              onSubmit={(values, { setSubmitting }) => {
+                console.log(values);
+              }}
+              validationSchema={userRegisterSchema}
+            >
+              {({ values, handleChange, handleSubmit, isSubmitting }) => (
+                <form onSubmit={handleSubmit} className="register">
+                  <h1 className="title">Реєстрація облікового запису</h1>
 
-            <button className="btn btn-outline-success">
-              Створити обліковий запис
-            </button>
-          </div> */}
+                  <label className="label">
+                    <AccountCircleIcon className="icon" />
+
+                    <FastField
+                      type="text"
+                      name="name"
+                      placeholder="Ваше імя:"
+                    />
+                    <ErrorMessage
+                      name="name"
+                      component="div"
+                      style={{ color: 'red', textTransform: 'upperCase' }}
+                    />
+                  </label>
+
+                  <label htmlFor="email" className="label">
+                    <MailOutlineIcon className="icon" />
+
+                    <FastField type="email" name="email" placeholder="Пошта:" />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      style={{ color: 'red', textTransform: 'upperCase' }}
+                    />
+                  </label>
+
+                  <label className="label">
+                    <PasswordIcon className="icon" />
+
+                    <FastField
+                      type="password"
+                      name="password"
+                      placeholder="Пароль:"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      style={{ color: 'red', textTransform: 'upperCase' }}
+                    />
+                  </label>
+
+                  <button
+                    className="btn btn-outline-success"
+                    type="submit"
+                    disabled={isSubmitting}
+                    style={{ width: '100%', marginTop: '30px' }}
+                  >
+                    Реєстрація
+                  </button>
+                </form>
+              )}
+            </Formik>
+          </div>
         </div>
       </div>
     </div>,
--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -1,6 +1,11 @@
 import * as yup from 'yup';
 
 export const userLoginSchema = yup.object({
+  email: yup.string().required().email(),
+  password: yup.string().required().min(7).max(14),
+});
+
+export const userRegisterSchema = yup.object({
   name: yup.string().required(),
   email: yup.string().required().email(),
   password: yup.string().required().min(7).max(14),
