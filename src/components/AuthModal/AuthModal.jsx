a05fb6a94f2e18bbdee88fac9b8a8a7546473700
VasylMolnar <vasylmolnar281@gmail.com>
2023-03-23 10:32:26 +0200

    new

--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -1,13 +1,96 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, FastField, ErrorMessage } from 'formik';
+import { userLoginSchema } from '../../utils/validationSchema';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import PasswordIcon from '@mui/icons-material/Password';
 
 const AuthModal = ({ isLogIn, setIsLogIn }) => {
-  console.log(isLogIn);
-  return (
+  return ReactDOM.createPortal(
     <div className={isLogIn ? 'backdropAuth' : 'backdropAuth is-hidden'}>
       <div className="authModal">
-        <h1>hello</h1>
+        <div className="btn-list">
+          <button className="btn btn-primary">LogIn</button>
+          <button className="btn btn-primary">Register</button>
+        </div>
+
+        <div className="authModal_content">
+          <div className="section_logIn">
+            <Formik
+              initialValues={{ name: '', email: '', password: '' }}
+              onSubmit={(values, { setSubmitting }) => {
+                console.log(values);
+              }}
+              validationSchema={userLoginSchema}
+            >
+              {({ values, handleChange, handleSubmit, isSubmitting }) => (
+                <form onSubmit={handleSubmit} className="logIn">
+                  <h1 className="title">Вхід у обліковий запис</h1>
+                  <label htmlFor="name" className="label">
+                    <AccountCircleIcon className="icon" />
+
+                    <FastField
+                      type="text"
+                      name="name"
+                      placeholder="Ваше імя:"
+                    />
+                    <ErrorMessage
+                      name="name"
+                      component="div"
+                      style={{ color: 'red', textTransform: 'upperCase' }}
+                    />
+                  </label>
+
+                  <label htmlFor="email" className="label">
+                    <MailOutlineIcon className="icon" />
+
+                    <FastField type="email" name="email" placeholder="Пошта:" />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      style={{ color: 'red', textTransform: 'upperCase' }}
+                    />
+                  </label>
+
+                  <label htmlFor="password" className="label">
+                    <PasswordIcon className="icon" />
+
+                    <FastField
+                      type="password"
+                      name="password"
+                      placeholder="Пароль:"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      style={{ color: 'red', textTransform: 'upperCase' }}
+                    />
+                  </label>
+
+                  <button
+                    className="btn btn-outline-primary"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Увійти
+                  </button>
+                </form>
+              )}
+            </Formik>
+          </div>
+
+          {/* <div className="section_register">
+            <h1>register</h1>
+
+            <button className="btn btn-outline-success">
+              Створити обліковий запис
+            </button>
+          </div> */}
+        </div>
       </div>
-    </div>
+    </div>,
+    document.getElementById('authModal')
   );
 };
 
