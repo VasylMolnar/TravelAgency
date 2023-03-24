27a1dc1e1410281d66659a5bcbd12d0677a053ae
VasylMolnar <vasylmolnar281@gmail.com>
2023-03-24 10:04:26 +0200

    new

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
+import { Formik, FastField, ErrorMessage } from 'formik';
+import { userRegisterSchema } from '../utils/validationSchema';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import PasswordIcon from '@mui/icons-material/Password';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import Rooms from './Rooms';
+import Table from '../components/UI/Table/Table';
 
 const UserPage = () => {
+  // fetch user data from server
+
   return (
     <main className="section userPage">
       <div className="container">
@@ -15,6 +24,7 @@ const UserPage = () => {
               sx={{ width: 200, height: 200 }}
             />
 
+            <input className="custom-file-input" type="file" />
             <p
               style={{
                 marginTop: '30px',
@@ -26,11 +36,78 @@ const UserPage = () => {
           </div>
 
           <div className="userEdit">
-            <h1>edit</h1>
+            <Formik
+              initialValues={{
+                name: 'User1',
+                email: 'test123@gmail.com',
+                password: '123213123',
+              }} //select data from server
+              onSubmit={(values, { setSubmitting }) => {
+                console.log(values);
+              }}
+              validationSchema={userRegisterSchema}
+            >
+              {({ values, handleChange, handleSubmit, isSubmitting }) => (
+                <form onSubmit={handleSubmit} className="edit_profile">
+                  <h1 className="title">Редагувати профіль</h1>
+
+                  <label className="label">
+                    <AccountCircleIcon className="icon" />
+
+                    <FastField type="text" name="name" placeholder="Імя:" />
+                    <ErrorMessage
+                      name="name"
+                      component="div"
+                      style={{ color: 'red', textTransform: 'upperCase' }}
+                    />
+                  </label>
+
+                  <label className="label">
+                    <MailOutlineIcon className="icon" />
+
+                    <FastField type="email" name="email" placeholder="Пошта:" />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      style={{ color: 'red', textTransform: 'upperCase' }}
+                    />
+                  </label>
+
+                  <label className="label">
+                    <PasswordIcon className="icon" />
+
+                    <FastField
+                      type="password"
+                      name="password"
+                      placeholder="Пароль:"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      style={{ color: 'red', textTransform: 'upperCase' }}
+                    />
+                  </label>
+
+                  <button
+                    className="btn btn-outline-primary"
+                    type="submit"
+                    disabled={isSubmitting}
+                    style={{ width: '85%' }}
+                  >
+                    Змінити
+                  </button>
+                </form>
+              )}
+            </Formik>
           </div>
         </div>
 
-        <div className="hotel_content">{/* list of hotel */}</div>
+        <div className="hotel_content">
+          {/* list of hotel */}
+          {/* namehotel Rooms(Flor) datastart dataend allMoney */}
+          <Table />
+          {/* select table options from user data */}
+        </div>
         <div className="tickets_content">{/* list of tickets */}</div>
 
         <div className="gallery_content"></div>
